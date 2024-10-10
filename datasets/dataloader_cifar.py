19f00d8d08506dc87e2015c46f1bd8c4e56c4cdf
filiperobotic <filipe.rolim@gmail.com>
2024-10-10 15:30:40 -0300

    remove open_noise

--- datasets/dataloader_cifar.py
+++ datasets/dataloader_cifar.py
@@ -26,7 +26,7 @@ class cifar_dataset(Dataset):
         self.transform = transform
         self.mode = dataset_mode
         self.transition = {0: 0, 2: 0, 4: 7, 7: 7, 1: 1, 9: 1, 3: 5, 5: 3, 6: 6, 8: 8}  # class transition for asymmetric noise
-        self.open_noise = None
+        # self.open_noise = None
         self.closed_noise = None
 
         if self.mode == 'test':
@@ -67,18 +67,18 @@ class cifar_dataset(Dataset):
             if os.path.exists(noise_file):
                 noise = json.load(open(noise_file, "r"))
                 noise_labels = noise['noise_labels']
-                self.open_noise = noise['open_noise']
+                # self.open_noise = noise['open_noise']
                 # self.open_id = np.array(self.open_noise)[:, 0] if len(self.open_noise) !=0 else None
                 self.closed_noise = noise['closed_noise']
-                for cleanIdx, noisyIdx in noise['open_noise']:
-                    if noisy_dataset == 'imagenet32':
-                        self.cifar_data[cleanIdx] = np.asarray(
-                            Image.open('{}/{}.png'.format(noise_data_dir, str(noisyIdx + 1).zfill(7)))).reshape((32, 32, 3))
-                        # set the groundtruth outliers label to be -1
-                        self.clean_label[cleanIdx] = 10
-                    else:
-                        self.cifar_data[cleanIdx] = noise_data[noisyIdx]
-                        self.clean_label[cleanIdx] = 10
+                # for cleanIdx, noisyIdx in noise['open_noise']:
+                #     if noisy_dataset == 'imagenet32':
+                #         self.cifar_data[cleanIdx] = np.asarray(
+                #             Image.open('{}/{}.png'.format(noise_data_dir, str(noisyIdx + 1).zfill(7)))).reshape((32, 32, 3))
+                #         # set the groundtruth outliers label to be -1
+                #         self.clean_label[cleanIdx] = 10
+                #     else:
+                #         self.cifar_data[cleanIdx] = noise_data[noisyIdx]
+                #         self.clean_label[cleanIdx] = 10
                 self.cifar_label = noise_labels
             # if os.path.exists(noise_file):
             #     noise_label = json.load(open(noise_file,"r"))
@@ -89,18 +89,20 @@ class cifar_dataset(Dataset):
                 idx = list(range(50000))  # indices of cifar dataset
                 random.shuffle(idx)
                 num_total_noise = int(self.r * 50000)  # total amount of noise
-                num_open_noise = int(self.on * num_total_noise)  # total amount of noisy/openset images
+                #num_open_noise = int(self.on * num_total_noise)  # total amount of noisy/openset images
                 print('Statistics of synthetic noisy CIFAR dataset: ', 'num of clean samples: ', 50000 - num_total_noise,
-                      ' num of closed-set noise: ', num_total_noise - num_open_noise, ' num of open-set noise: ', num_open_noise)
+                      ' num of closed-set noise: ', num_total_noise , ' num of open-set noise: ')
+                    #   ' num of closed-set noise: ', num_total_noise - num_open_noise, ' num of open-set noise: ', num_open_noise)
                 # print(num_open_noise, num_total_noise)
-                if noisy_dataset == 'imagenet32':  # indices of openset source images
-                    target_noise_idx = list(range(1281149))
-                else:
-                    target_noise_idx = list(range(50000))
+                # if noisy_dataset == 'imagenet32':  # indices of openset source images
+                #     target_noise_idx = list(range(1281149))
+                # else:
+                target_noise_idx = list(range(50000))
                 random.shuffle(target_noise_idx)
-                self.open_noise = list(
-                    zip(idx[:num_open_noise], target_noise_idx[:num_open_noise]))  # clean sample -> openset sample mapping
-                self.closed_noise = idx[num_open_noise:num_total_noise]  # closed set noise indices
+                # self.open_noise = list(
+                #     zip(idx[:num_open_noise], target_noise_idx[:num_open_noise]))  # clean sample -> openset sample mapping
+                # self.closed_noise = idx[num_open_noise:num_total_noise]  # closed set noise indices
+                self.closed_noise = idx[0:num_total_noise]  # closed set noise indices
                 # populate noise_labels
                 for i in range(50000):
                     if i in self.closed_noise:
@@ -115,18 +117,19 @@ class cifar_dataset(Dataset):
                     else:
                         noise_labels.append(cifar_label[i])
                 # populate openset noise images
-                for cleanIdx, noisyIdx in self.open_noise:
-                    if noisy_dataset == 'imagenet32':
-                        self.cifar_data[cleanIdx] = np.asarray(
-                            Image.open('{}/{}.png'.format(noise_data_dir, str(noisyIdx + 1).zfill(7)))).reshape((32, 32, 3))
-                        self.clean_label[cleanIdx] = 10000
+                # for cleanIdx, noisyIdx in self.open_noise:
+                #     # if noisy_dataset == 'imagenet32':
+                #     #     self.cifar_data[cleanIdx] = np.asarray(
+                #     #         Image.open('{}/{}.png'.format(noise_data_dir, str(noisyIdx + 1).zfill(7)))).reshape((32, 32, 3))
+                #     #     self.clean_label[cleanIdx] = 10000
 
-                    else:
-                        self.cifar_data[cleanIdx] = noise_data[noisyIdx]
-                        self.clean_label[cleanIdx] = 10000
+                #     # else:
+                #     self.cifar_data[cleanIdx] = noise_data[noisyIdx]
+                #     self.clean_label[cleanIdx] = 10000
 
                 # write noise to a file, to re-use
-                noise = {'noise_labels': noise_labels, 'open_noise': self.open_noise, 'closed_noise': self.closed_noise}
+                #noise = {'noise_labels': noise_labels, 'open_noise': self.open_noise, 'closed_noise': self.closed_noise}
+                noise = {'noise_labels': noise_labels,  'closed_noise': self.closed_noise}
                 print("save noise to %s ..." % noise_file)
                 json.dump(noise, open(noise_file, "w"))
                 self.cifar_label = noise_labels
@@ -157,8 +160,9 @@ class cifar_dataset(Dataset):
     def __len__(self):
         return len(self.cifar_data)
 
-    def get_noise(self):
-        return (self.open_noise, self.closed_noise)
+    # def get_noise(self):
+    #     #return (self.open_noise, self.closed_noise)
+    #     return (None, self.closed_noise)
 
     def __repr__(self):
         return f'dataset_mode: {self.mode}, dataset number: {len(self)} \n'
