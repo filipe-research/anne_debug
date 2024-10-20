afce162670537e4b35a8eb90e0f8bd162754097a
filiperobotic <filipe.rolim@gmail.com>
2024-10-20 11:23:03 -0300

    chunks

--- main_cifar_ANNE_light.py
+++ main_cifar_ANNE_light.py
@@ -50,6 +50,7 @@ parser.add_argument('--exp-name', type=str, default='')
 #              'otsu_rad3','otsu_rad4', 'otsu_rad5', 'otsu_rad_inv' ])
 parser.add_argument('--warmup', default=0, type=int, metavar='wm', help='number of total warmup')
 parser.add_argument('--kmax', default=200, type=int, metavar='kmax', help='maximum value of ')
+parser.add_argument('--chunks', default=200, type=int, metavar='kmax', help='maximum value of ')
 
 parser.add_argument('--distill_mode', type=str, default='fine-gmm', choices=['fine-kmeans','fine-gmm'], help='mode for distillation kmeans or gmm.')
 
@@ -176,13 +177,13 @@ def evaluate(dataloader, encoder, classifier, args, noisy_label, clean_label, i,
                 #prediction_knn = fast_weighted_knn_ball(i, feature_bank[aknn_ids], feature_bank[aknn_ids], modified_label[aknn_ids], args.num_classes, args.k,  rule=args.rule, conf=his_score,  radaptive=args.radaptive, otsu_split=otsu_split, kmax=args.kmax, kmin1=args.kmin1, kmin2=args.kmin2 )  # temperature in weighted KNN
                 #prediction_knn = fast_weighted_knn_ball(i, feature_bank[aknn_ids], feature_bank[aknn_ids], modified_label[aknn_ids], args.num_classes, args.k,  rule=args.rule, conf=his_score,  radaptive=args.radaptive, otsu_split=otsu_split, kmax=args.kmax, kmin1=args.kmin1, kmin2=args.kmin2 )  # temperature in weighted KNN
                 #prediction_knn = fast_weighted_knn_ball( feature_bank[aknn_ids], feature_bank[aknn_ids], modified_label[aknn_ids], args.num_classes, chunks=200,  rule=args.rule,   radaptive=args.radaptive, otsu_split=otsu_split, kmax=args.kmax, kmin1=args.kmin1, kmin2=args.kmin2 )  # temperature in weighted KNN
-                prediction_knn = fast_weighted_knn_ball( feature_bank[aknn_ids], feature_bank[aknn_ids], modified_label[aknn_ids], args.num_classes, chunks=200,   otsu_split=otsu_split, kmax=args.kmax, kmin1=args.kmin1, kmin2=args.kmin2, step=args.step )  # temperature in weighted KNN
+                prediction_knn = fast_weighted_knn_ball( feature_bank[aknn_ids], feature_bank[aknn_ids], modified_label[aknn_ids], args.num_classes, chunks=args.chunks,   otsu_split=otsu_split, kmax=args.kmax, kmin1=args.kmin1, kmin2=args.kmin2, step=args.step )  # temperature in weighted KNN
                 vote_y = torch.gather(prediction_knn, 1, modified_label[aknn_ids].view(-1, 1)).squeeze()
 
             elif args.ssrset == "full":
                 #prediction_knn = fast_weighted_knn_ball(i, feature_bank, feature_bank, modified_label, args.num_classes, args.k,  rule=args.rule, conf=his_score,  radaptive=args.radaptive, otsu_split=otsu_split, kmax=args.kmax, kmin1=args.kmin1, kmin2=args.kmin2 )  # temperature in weighted KNN
                 #prediction_knn = fast_weighted_knn_ball( feature_bank, feature_bank, modified_label, args.num_classes, chunks=200,  rule=args.rule,  radaptive=args.radaptive, otsu_split=otsu_split, kmax=args.kmax, kmin1=args.kmin1, kmin2=args.kmin2 )  # temperature in weighted KNN
-                prediction_knn = fast_weighted_knn_ball( feature_bank, feature_bank, modified_label, args.num_classes, chunks=200, otsu_split=otsu_split, kmax=args.kmax, kmin1=args.kmin1, kmin2=args.kmin2,  step=args.step )  # temperature in weighted KNN
+                prediction_knn = fast_weighted_knn_ball( feature_bank, feature_bank, modified_label, args.num_classes, chunks=args.chunks, otsu_split=otsu_split, kmax=args.kmax, kmin1=args.kmin1, kmin2=args.kmin2,  step=args.step )  # temperature in weighted KNN
                 vote_y = torch.gather(prediction_knn, 1, modified_label.view(-1, 1)).squeeze()
         else:
             #prediction_knn = weighted_knn(feature_bank, feature_bank, modified_label, args.num_classes, args.k, 10)
